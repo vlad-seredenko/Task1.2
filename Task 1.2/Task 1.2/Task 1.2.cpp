ef68d6a5c60d923836d2b6b21b0fb6a55dd8a61b
vlad-seredenko <78535638+vlad-seredenko@users.noreply.github.com>
2021-05-21 00:11:57 +0300

    Add files via upload

---
+++ Task 1.2/Task 1.2/Task 1.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+double Summ(double x, double E)
+{
+    int n = 0;
+    double a = 1;
+    double y_cos_x = a;
+    double cos_x;
+
+    do {
+        cos_x = -x * x / ((2. * n + 1.) * (2. * n + 2.));
+        a *= cos_x;
+        y_cos_x += a;
+        n++;
+
+    } while (fabs(a) < E);
+    return y_cos_x;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double x;
+    double E;
+    cout << "Введiть Е = ";
+    cin >> E;
+    cout << "Введiть х = ";
+    cin >> x;
+    double y_cos_x = Summ(x, E);
+    cout<<"y = cos(x) = " << y_cos_x << endl;
+    return 0;
+}
